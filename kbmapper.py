9be831ef63e2baee278db712699cd65c627ca1c7
Nate Symer <nate@symer.io>
2016-12-03 16:21:27 -0500

    Improve quality of kbmapper event handling

--- kbmapper.py
+++ kbmapper.py
@@ -38,34 +38,18 @@ import sys
 import os
 import signal
 
+#
+## XKB Keysyms
+#
+
 ctx = xkb.Context()
 keymap = ctx.keymap_new_from_names()
 state = keymap.state_new()
 
-def ls(direc):
-  for dpth, _, fnames in os.walk(direc):
-    for f in fnames:
-      yield os.path.abspath(os.path.join(dpth, f))
-
-def load_config(f):
-  c = configparser.ConfigParser()
-  c.read(f)
-  return c
-
-configs = list(map(load_config, ls("/etc/kbmapper/kbmapper.d/").append("/etc/kbmapper/kbmapper.conf")))
-
-def exec_config(key):
-  for cfg in configs:
-    if k in cfg:
-      v = cfg[k]
-      cmd = v.get("Exec", None)
-      if cmd is not None:
-        print(os.system(cmd))
-        return True
-  return False
-      
-
 def map_keycode(keycode):
+  if type(keycode) is str:
+    return keycode
+  
   raw = xkb.keysym_get_name(state.key_get_one_sym(keycode + keymap.min_keycode() - 1))
   if raw == "NoSymbol":
     return None
@@ -74,59 +58,102 @@ def map_keycode(keycode):
   else:
     return raw
 
-def emit_keystroke(keycodes):
-  return exec_config('+'.join(list(map(map_keycode,keycodes))))
+#
+## Configuration
+#
 
-def emit_lid(closed=False):
-  return exec_config("lid_close" if closed else "lid_open")
+def ls(direc):
+  xs = []
+  for dpth, _, fnames in os.walk(direc):
+    for f in fnames:
+      xs.append(os.path.abspath(os.path.join(dpth, f)))
+  return xs
 
-def emit_headphone(plugged_in=False):
-  return exec_config("headphones_" + ("in" if pluggen_in else "out"))
+def load_config(f):
+  c = configparser.ConfigParser()
+  c.read(f)
+  return c
+
+paths = ls("/etc/kbmapper/kbmapper.d/")
+paths.append("/etc/kbmapper/kbmapper.conf")
+configs = list(map(load_config, paths))
+
+#
+## Event Handling
+#
+
+# TODO: order-independent config matching
+def emit(keysym):
+  for cfg in configs:
+    if keysym in cfg:
+      v = cfg[keysym]
+      cmd = v.get("Exec", None) # Command to execute on event
+      if cmd is not None:
+        user = v.get("User", None) # User to execute command as
+        workdir = v.get("WorkingDirectory", None) # Directory to execute command in
+        print(os.system(cmd))
+        return True
+  return False
+
+#
+## Event Implementation
+#
 
 keystack = []
-cur_ev = None
 
-# TODO: key events need to be synchronized!!!
-# (or maybe synchronization makes keys too slow!)
+def handle_event(code, typ, value):
+  global keystack
+  print(code, typ, value)
+  if typ is 1: # Key action
+    if value is 1: # press down
+      if code not in keystack:
+        keystack.append(code)
+    elif value is 0: # lift up
+      if code in keystack:
+        if emit('+'.join(list(map(map_keycode,keystack)))):
+          keystack = []
+        else:
+          keystack.remove(code)
+  elif typ is 5:
+    if code is 0: # lid event
+      emit("LidOpen" if value is 0 else "LidClose")
+    elif code is 2: # headphone jack event
+      emit("HeadphonesIn" if value is 1 else "HeadphonesOut")
+
 @asyncio.coroutine
 def listen_events(d):
-  global keystack
-  global cur_ev
+  cur_ev = None
   while True:
     events = yield from d.async_read()
     for e in events:
-      if e.type is 1: # Key action
-        if e.value is 1: # press down
-          if e.code not in keystack:
-            keystack.append(e.code)
-        elif e.value is 0: # lift up
-          if e.code in keystack:
-            if emit_keystroke(keystack):
-              keystack = []
-            else:
-              keystack.remove(e.code)
-      elif e.type is 5:
-        cur_ev = (e.code, e.value)
-      elif e.type is 0:
-        if cur_ev is not None and cur_ev[0] is 0: # lid event
-          emit_lid(cur_ev[1] == 0)
-        if cur_ev is not None and cur_ev[0] is 2: # headphone jack event 
-          emit_headphone(cur_ev[1] is 1)
-        cur_ev = None
+      if e.type is 0: # EV_SYN
+        if cur_ev is not None:
+          # TODO: handle EV_SYN behavior
+          handle_event(cur_ev.code, cur_ev.type, cur_ev.value)
+          cur_ev = None
+      else:
+        cur_ev = e
 
-for fn in evdev.list_devices():
-  d = evdev.InputDevice(fn)
-  c = d.capabilities()
-  if 1 in c or 5 in c: # 1 is EV_KEY and 5 is EV_SW
-    asyncio.async(listen_events((d)))
+#
+## Main
+#
 
-loop = asyncio.get_event_loop()
+def main(args):
+  for fn in evdev.list_devices():
+    d = evdev.InputDevice(fn)
+    c = d.capabilities()
+    if 1 in c or 5 in c: # 1 is EV_KEY and 5 is EV_SW
+      asyncio.async(listen_events((d)))
+
+  loop = asyncio.get_event_loop()
   
-def signal_handler(signal, frame):
-  loop.stop()
-  sys.exit(0)
+  def signal_handler(signal, frame):
+    loop.stop()
+    sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
-signal.signal(signal.SIGTERM, signal_handler)
+  signal.signal(signal.SIGINT, signal_handler)
+  signal.signal(signal.SIGTERM, signal_handler)
 
-loop.run_forever()
+  loop.run_forever()
+
+main(sys.argv)
